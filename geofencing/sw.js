0e4a9c65ee4ad4b56e38c2ab11f5219c519ccb5b
Marijn Kruisselbrink <mek@google.com>
2014-10-16 15:30:34 -0700

    sw notif

--- geofencing/sw.js
+++ geofencing/sw.js
@@ -6,12 +6,15 @@ this.oninstalled = function(e) {
 
 this.ongeofenceenter = function(e) {
   console.log("geofence enter ", e);
+  var n = new Notification("Geofence entered");
 };
 
 this.ongeofencelaeve = function(e) {
   console.log("geofence leave ", e);
+  var n = new Notification("Geofence left");
 };
 
 this.onactivate = function(e) {
   console.log("activate ", e);
+  var n = new Notification("SW activated");
 };
