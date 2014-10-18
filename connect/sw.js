a895f78f53b3546210643b97f36603667d809c25
Marijn Kruisselbrink <mkruisselbrink@kde.org>
2014-10-17 20:50:49 -0700

    bla

--- connect/sw.js
+++ connect/sw.js
@@ -1,2 +1,14 @@
 
-importScripts("service.js");
+//importScripts("service.js");
+
+
+var connectableUrls;
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request);
+  // for now assume all urls are connectable
+  event.respondWith(
+    new Response("<!DOCTYPE html><html><body>foo</body></html>",
+                 {headers: 'content-type: text/html'});
+  );
+});
