c72e1d056f8e765b5ff61950ab4503bd03b85604
Marijn Kruisselbrink <mkruisselbrink@kde.org>
2014-10-17 21:15:20 -0700

    ja

--- connect/sw.js
+++ connect/sw.js
@@ -10,7 +10,18 @@ self.addEventListener('fetch', function(event) {
   console.log(event.request);
   // for now assume all urls are connectable
   event.respondWith(
-    new Response("<!DOCTYPE html><html><script>window.onmessage = function(e) { console.log(e); };</script><body>foo</body></html>",
+    new Response("<!DOCTYPE html><html><script>window.onmessage = function(e) { console.log(e); navigator.serviceWorker.controller.postMessage(e.data, e.ports); };</script><body>foo</body></html>",
                  {headers: {'content-type': 'text/html'}})
   );
+});
+
+self.addEventListener('message', function(event) {
+  console.log(event);
+  
+  if (event.data == 'connect') {
+    var port = event.ports[0];
+    console.log(port);
+    port.onmessage = function(e) { console.log(e); };
+    port.postMessage('welcome');
+  }
 });
