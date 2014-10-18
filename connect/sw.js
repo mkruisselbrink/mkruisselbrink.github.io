7770c0465a995ca60ddade4a66c3b1945bfaf4b5
Marijn Kruisselbrink <mkruisselbrink@kde.org>
2014-10-18 10:49:17 -0700

    bla

--- connect/sw.js
+++ connect/sw.js
@@ -1,9 +1,11 @@
+
 self.addEventListener('fetch', function(event) {
   console.log(event.request);
   // for now assume all urls are connectable
   event.respondWith(
     new Response("<!DOCTYPE html><html><script>" + 
       "window.onmessage = function(e) { " + 
+      "console.log(e);" +
         "var port = e.ports[0]; " +
         "port.onmessage = function(ep) { " +
           "navigator.serviceWorker.controller.postMessage({foreignMessage: ep.data}, ep.ports); " +
@@ -15,4 +17,21 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('message', function(event) {
   console.log(event);
+  if ('foreignMessage' in event.data) {
+    var foreignMessageEvent = {
+      data: event.data['foreignMessage'],
+      ports: event.ports,
+      source: {
+        postMessage = function(msg, transfer) {
+          event.source.postMessage(msg, transfer);
+        }
+      }
+    };
+    onForeignMessage(foreignMessageEvent);
+  }
 });
+
+function onForeignMessage(event) {
+  console.log(event);
+  event.source.postMessage("Response from SW");
+}
