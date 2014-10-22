6672946a4f26fa02d281cb8d33e8f53a172162ad
Marijn Kruisselbrink <mek@google.com>
2014-10-22 16:17:32 -0700

    l

--- connect/sw.js
+++ connect/sw.js
@@ -1,6 +1,8 @@
+var exposed_service = "https://mkruisselbrink.github.io/connect/services/test";
+
 self.addEventListener('foreignconnect', function(event) {
   console.log(event);
-
+  event.acceptConnection(event.targetUrl == exposed_service);
 });
 
 self.addEventListener('fetch', function(event) {
