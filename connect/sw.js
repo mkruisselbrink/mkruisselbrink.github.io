4e7db1afb5e1e6f0ae27e1cd6ef4a5a178de2136
Marijn Kruisselbrink <mek@google.com>
2014-11-03 13:07:40 -0800

    update polyfill

--- connect/sw.js
+++ connect/sw.js
@@ -1,3 +1,5 @@
+importScripts('service-polyfill.js');
+
 var exposed_service = "https://mkruisselbrink.github.io/connect/services/test";
 
 self.addEventListener('foreignconnect', function(event) {
@@ -5,49 +7,8 @@ self.addEventListener('foreignconnect', function(event) {
   event.acceptConnection(event.targetUrl == exposed_service);
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log(event.request);
-  // for now assume all urls are connectable
-  event.respondWith(
-    new Response("<!DOCTYPE html><html><script>" + 
-      "var port;" +
-      "window.onmessage = function(e) {" + 
-        "if (e.data == 'connect') {" +
-          "port = e.ports[0]; " +
-          "port.onmessage = function(ep) { " +
-            "navigator.serviceWorker.controller.postMessage({foreignMessage: ep.data}, ep.ports); " +
-          "}; " +
-        "} else if ('reply' in e.data) {" +
-          "port.postMessage(e.data['reply'], e.ports);" +
-        "}" +
-      "};</script></html>",
-                 {headers: {'content-type': 'text/html'}})
-  );
-});
-
-self.addEventListener('message', function(event) {
-  console.log(event);
-  if ('foreignMessage' in event.data) {
-    var foreignMessageEvent = {
-      data: event.data['foreignMessage'],
-      ports: event.ports,
-      source: {
-        postMessage: function(msg, transfer) {
-          // TODO: should just reply to event.source. but that doesn't exist
-          //    so for now respond to the first client
-          // event.source.postMessage(msg, transfer);
-          self.clients.getAll()
-            .then(function(clients) {
-                clients[0].postMessage({reply: msg}, transfer);
-              });
-        }
-      }
-    };
-    onForeignMessage(foreignMessageEvent);
-  }
-});
-
-function onForeignMessage(event) {
+self.addEventListener('foreignmessage', function(event) {
   console.log(event);
   event.source.postMessage("Response from SW");
-}
+});
+
